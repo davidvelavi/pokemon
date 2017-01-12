754a9855257f0105b45e7cb65958b03991fe72a2
David Velazquez <dvelazquezvillegas@hotmail.com>
2017-01-11 20:52:36 -0600

    Creacion de detalle pokemon y evolucion

--- app.js
+++ app.js
@@ -54,16 +54,23 @@ modulo = angular.module("app",['ui.router']);
 
 var PokemonDetalleController = function($scope,$http,detalle){
         $scope.detallePokemon = {};
+        var evolucion = [];
         $scope.detallePokemon.nombre = detalle[0].name;
         $scope.detallePokemon.img = './img/pokemons/'+detalle[0].name.toLowerCase()+'.jpg';
         $scope.detallePokemon.especie = detalle[0].species;
         $scope.detallePokemon.tipo = detalle[0].type;
         $scope.detallePokemon.altura = detalle[0].height;
         $scope.detallePokemon.peso = detalle[0].weight;
-        $scope.detallePokemon.evolucion = detalle[0].evolution.filter(function(current){
+        evolucion = detalle[0].evolution.filter(function(current){
             return current != detalle[0].name;
         });
-
+        $scope.detallePokemon.evolucion = [];
+        for(var i in evolucion){
+            var evolucionDetalle = {};
+            evolucionDetalle.img = './img/pokemons/'+evolucion[i].toLowerCase()+'.jpg';
+            evolucionDetalle.nombre = evolucion[i];
+            $scope.detallePokemon.evolucion.push(evolucionDetalle);
+        }
     }
 
 
